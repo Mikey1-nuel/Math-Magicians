630411fcd991a4de023bd1284964c729b9359884
Emmanuel <emmanuelnwoye12@gmail.com>
2023-05-03 19:23:55 +0100

    Update js file

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,20 +2,26 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 
 const Nav = () => (
-    <nav className='nav'>
-      <h1>Math Magician</h1>
-      <ul className='menu'>
+  <nav className='nav'>
+    <h1>Math Magician</h1>
+    <ul className='menu'>
       <Link to="/">
-      <li title="home">Home</li>
-    </Link>
-    <Link to="/calculator">
-    <li title="calculator">Calculator</li>
-  </Link>
-  <Link to="/quotes">
-  <li title="quotes">Quotes</li>
-</Link>
-      </ul>
-    </nav>
+        <li className='navlink' title="home">
+          Home
+        </li>
+      </Link>
+      <Link to="/calculator">
+        <li className='navlink' title="calculator">
+          Calculator
+        </li>
+      </Link>
+      <Link to="/quotes">
+        <li className='navlink' title="quotes">
+          Quotes
+        </li>
+      </Link>
+    </ul>
+  </nav>
 );
 
 export default Nav;
